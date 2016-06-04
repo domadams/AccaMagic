e71ea0a1f4d4d5db9f834c3401345a79baae4eb0
Dom Adams <dominic.adams@uk.tesco.com>
2016-06-04 11:25:02 +0200

    Update readme for node6

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import config from 'config';
 import app from './app';
 
-let server = app().listen(
+const server = app().listen(
     config.get('port'),
     config.get('host'),
     () => console.log(`AccaMagic started ${JSON.stringify(server.address())}`)
